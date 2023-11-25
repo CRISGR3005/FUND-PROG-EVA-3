6c8ce7762686c406a200e4c4d75c8c76457abfc2
CRISGR3005 <143569144+CRISGR3005@users.noreply.github.com>
2023-11-24 23:22:14 -0600

    Add files via upload

---
+++ EVA3_5_REFRENCIAS/src/eva3_5_refrencias/EVA3_5_REFRENCIAS.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva3_5_refrencias;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Criatian Ulises
+ */
+public class EVA3_5_REFRENCIAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] arreglo = new int [10];
+        System.out.println(arreglo);
+        for (int i: arreglo){
+        System.out.println(i);  
+    }
+        String[][] arreCade = new String [10][10];
+        System.out.println(arreCade);
+        Scanner input = new Scanner (System.in);
+        System.out.println(input);
+    
+ }
+}
